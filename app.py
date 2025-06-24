3c11487f322247c9fbea0aeb8639545c0af327a7
kaplimonadi <kapilmonadi@gmail.com>
2025-06-24 13:22:36 +0530

    introduced lang chain and chat model

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-import gradio as gd
+import gradio as gr
+from langchain_ollama import ChatOllama
+from langchain.globals import set_debug, set_verbose
+
+set_debug(True) # enable debugging in Langchain
+set_verbose(True)
+
+llm = ChatOllama(model="llama3.2")
 
 def sayHello(name: str):
     return f"Hello {name}"
 
-ui=gd.Interface(fn=sayHello, inputs="textbox", outputs="textbox", allow_flagging="never")
+def get_response(query: str) -> str:
+    response = llm.invoke(query)
+    return response.content
+
+# ui=gr.Interface(fn=sayHello, inputs="textbox", outputs="textbox", allow_flagging="never")
+ui=gr.Interface(fn=get_response, inputs=[gr.Textbox(label="Input query", lines=5)],
+                outputs=[gr.Textbox(label="Response", lines=10)], flagging_mode="never")
 ui.launch(share=True)
