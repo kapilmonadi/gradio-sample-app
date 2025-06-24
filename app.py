dfc6d506110c0ac382bdd58ab9bb28a132c49852
kaplimonadi <kapilmonadi@gmail.com>
2025-06-24 12:52:54 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import gradio as gd
 def sayHello(name: str):
     return f"Hello {name}"
 
-ui=gd.Interface(fn=sayHello, inputs="textbox", outputs="textbox")
-ui.launch()
+ui=gd.Interface(fn=sayHello, inputs="textbox", outputs="textbox", allow_flagging="never")
+ui.launch(share=True)
